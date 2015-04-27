d2e5f5ed937bedce184b70292a98d08bb85e0e0f
jerryli <jerryli_vip@163.com>
2015-04-27 18:23:01 +0800

    add applicationContext.xml

---
+++ src/main/java/com/pactera/web/model/Department.java
@@ -0,0 +1,47 @@
+package com.pactera.web.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dept")
+public class Department {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "deptno")
+	private Integer id;
+
+	@Column(name = "dname")
+	private String deptName;
+
+	private String loc;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	public String getLoc() {
+		return loc;
+	}
+
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+}
